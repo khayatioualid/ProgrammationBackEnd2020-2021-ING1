59975383a40329e7bce3be7b1e41810a287f0a89
khayati <oualid.khayati@gmail.com>
2020-11-06 15:45:56 +0100

    requete post et get

--- ExpressExemples/HelloWorld.js
+++ ExpressExemples/HelloWorld.js
@@ -1,11 +1,29 @@
+var bodyParser = require('body-parser');
+
 
 const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json()); // support json encoded bodies
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/somme', (req, res) => {
+  let x=parseFloat(req.query.x)
+  let y=parseFloat(req.query.y)
+  res.send('Hello World get somme = '+(x+y))
+})
+
+app.post('/somme', (req, res) => {
+  let x=parseFloat(req.body.x)
+  let y=parseFloat(req.body.y)
+  res.send('Hello World post somme = '+(x+y))
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
